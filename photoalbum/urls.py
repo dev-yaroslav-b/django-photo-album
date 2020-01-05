e951ed2f92654bbe27a0b6344fa9393767410ff5
dev-yaroslav-b <dev.yaroslav.b@gmail.com>
2020-01-05 16:16:48 +0200

    Minor refactoring

--- photoalbum/urls.py
+++ photoalbum/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path(r'', include('album.urls')),
+    path('', include('album.urls')),
     path('admin/', admin.site.urls),
 ]
