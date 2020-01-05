e6da86f7d6dc08af041d81efb49102fb64d5e0d5
dev-yaroslav-b <dev.yaroslav.b@gmail.com>
2020-01-05 16:15:21 +0200

    Minor refactoring

--- album/urls.py
+++ album/urls.py
@@ -7,13 +7,13 @@ from . import forms
 from photoalbum import settings
 
 urlpatterns = [
-                  url(r'^$', views.index, name='index'),
-                  url(r'^post/(?P<pk>\d+)/$', views.details, name='details'),
-                  url(r'^post/new/$', views.new_post, name='new_post'),
-                  url(r'^post/(?P<pk>\d+)/edit/$', views.edit_post, name='edit_post'),
-                  url(r'^post/share-video/$', views.share_video, name='share_video'),
-                  path(r'post/(?P<pk>\d+)/delete_post/', views.delete_post, name='delete_post'),
+    url(r'^$', views.index, name='index'),
+    url(r'^post/(?P<pk>\d+)/$', views.details, name='details'),
+    url(r'^post/new/$', views.new_post, name='new_post'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.edit_post, name='edit_post'),
+    url(r'^post/share-video/$', views.share_video, name='share_video'),
+    url(r'^post/(?P<pk>\d+)/delete_post/$', views.delete_post, name='delete_post'),
 
-                  url(r'^signup/$', forms.RegisterFormView.as_view()),
-                  url(r'^accounts/', include('django.contrib.auth.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^signup/$', forms.RegisterFormView.as_view()),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
