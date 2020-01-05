56d80fb7a2806ab366dee539b78e6977da2a4109
dev-yaroslav-b <dev.yaroslav.b@gmail.com>
2020-01-05 17:38:35 +0200

    Minor refactoring

--- album/models.py
+++ album/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 def user_directory_path(instance, filename):
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
+    return f'user_{instance.user.id}/{filename}'
 
 
 class MediaPost(models.Model):
@@ -15,17 +15,17 @@ class MediaPost(models.Model):
     media = models.FileField(upload_to=user_directory_path)
     created_date = models.DateTimeField(auto_now_add=False)
 
-    def extension(self):
-        name, extension = os.path.splitext(self.media.name)
-        if extension in ['.jpg', '.jpeg','.png']:
-            return 'img'
-        elif extension in ['.mp4', '.mkv']:
-            return 'video'
-        else:
-            None
-
     class Meta:
         ordering = ('title', 'created_date')
 
     def __str__(self):
         return self.title
+
+    def extension(self):
+        name, extension = os.path.splitext(self.media.name)
+        if extension in ['.jpg', '.jpeg', '.png']:
+            return 'img'
+        elif extension in ['.mp4', '.mkv']:
+            return 'video'
+        else:
+            None
